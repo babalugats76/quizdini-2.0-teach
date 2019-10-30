122460e29ed4071ea5e7780142b157d02af638f2
James Colestock <james@colestock.com>
2019-10-29 19:22:46 -0600

    FAQ page

--- client/src/components/About/FAQ.js
+++ client/src/components/About/FAQ.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import { Header, Segment, Table } from 'semantic-ui-react';
 
 export default props => (
   <Segment {...props}>
@@ -7,39 +7,108 @@ export default props => (
       Frequently Asked Questions
     </Header>
     <br />
-    <details style={{ marginBottom: '2em' }}>
-      <summary
-        style={{
-          fontSize: '1.71428571rem',
-          fontFamily: 'marcher-semibold',
-          marginBottom: '.5em'
-        }}
-      >
-        Who should use it?
-      </summary>
-      <p style={{ fontSize: '1.1em' }}>
-        As an evolving tool, Quizdini allows YOU to create the material your
-        students will use. Will we write your questions for you? No. Will we
-        support you as you build a collection of activities that help your
-        students learn, practice, and review? Heck, yes!
+    <details>
+      <summary>What is Quizdini?</summary>
+      <p>
+        Quizdini is an&nbsp;<strong>online learning tool</strong> that was
+        created by James Colestock in 2013. In those days, there was a dearth of
+        edtech tools available to provide students with practice opportunities
+        and which gave teachers the ability to gauge their progress.
+      </p>
+      <p>
+        A practice and learning tool made&nbsp;
+        <strong>by teachers for teachers</strong>, Quizdini is targeted at
+        educators of all content areas and levels. Quizdini is browser-based and
+        works on most any classroom device/up-to-date browser combination.
+        Designed to be&nbsp;<strong>simple</strong>,&nbsp;
+        <strong>straightforward</strong>, and to protect students&apos; privacy,
+        Quizdini is something that any teacher can put to productive use,&nbsp;
+        <strong>quickly</strong>.
       </p>
     </details>
-    <details style={{ marginBottom: '2em' }}>
-      <summary
-        style={{
-          fontSize: '1.71428571rem',
-          fontFamily: 'marcher-semibold',
-          marginBottom: '.5em'
-        }}
-      >
-        Who should use it?
-      </summary>
-      <p style={{ fontSize: '1.1em' }}>
-        As an evolving tool, Quizdini allows YOU to create the material your
-        students will use. Will we write your questions for you? No. Will we
-        support you as you build a collection of activities that help your
-        students learn, practice, and review? Heck, yes!
+    <details>
+      <summary>What Can I Do With Quizdini?</summary>
+      <p>
+        The possibilities are endless; after all, creativity is in the
+        constraints. Quizdini shines as reinforcement in that sweet spot between
+        introduction and application of new material. In practice, here are the
+        most popular use cases:
       </p>
+      <Table celled stackable collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Use Case</Table.HeaderCell>
+            <Table.HeaderCell>Example</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <strong>Class Competitions</strong>
+            </Table.Cell>
+            <Table.Cell>Design a group race or row challenge in which students engage in friendly competition</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>
+              <strong>Whole-Class Review</strong>
+            </Table.Cell>
+            <Table.Cell>
+              Use your projector or interactive whiteboard as part of a teacher- or student-led think-aloud
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>
+              <strong>Individualized Instruction</strong>
+            </Table.Cell>
+            <Table.Cell>
+              Build an activity, customized for a struggling student
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>
+              <strong>Unit Review</strong>
+            </Table.Cell>
+            <Table.Cell>Take your class to the computer lab for a day of study and reinforcement prior to &quot;The Big Test&quot;</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </details>
+    <details>
+      <summary>How do I create an activity?</summary>
+      <p>
+        Pick the game type. Currently, 'match' is the only option. Enter game
+        information, e.g., enter terms/definitions, upload vocab list, etc.,
+        save your work, share the link; alternatively use a URL shortener
+        (perhaps include links in body of this response)
+      </p>
+    </details>
+    <details>
+      <summary>What are the technical requirements?</summary>
+      <p>
+        Computing device. Internet connection. Up-to-date browser. Whilelist of
+        quizdini domains inside your oppressive educational organization.
+      </p>
+    </details>
+    <details>
+      <summary>What does it cost?</summary>
+      <p>
+        Explain credit-based, pay-to-create model. Refer to philosophy and
+        include verbiage about how credits cannot last forever.
+      </p>
+    </details>
+    <details>
+      <summary>Why can't I see my students' results?</summary>
+      <p style={{ fontSize: '1.1em' }}>
+        Privacy. Refer to Formative in Philosophy.
+      </p>
+    </details>
+    <details>
+      <summary>Can I create student accounts? Shared accounts?</summary>
+      <p style={{ fontSize: '1.1em' }}>No and no...explanation</p>
+    </details>
+    <details>
+      <summary>Quizdini looks different. What's new?</summary>
+      <p>Short list of what is new</p>
     </details>
   </Segment>
 );
--- client/src/components/About/Philosophy.js
+++ client/src/components/About/Philosophy.js
@@ -28,9 +28,10 @@ export default props => (
           constraints force your brain to be more creative
         </a>
         . Consider music: there are just 12 tones in the chromatic scale. How
-        about nature? A handful of organic compounds comprise most life. Aren&apos;t
-        sports just a ball and some rules? And Oregami? Doesn&apos;t the same flat,
-        square sheet of paper yield infinite sculptural possibilities?
+        about nature? A handful of organic compounds comprise most life.
+        Aren&apos;t sports just a ball and some rules? And Oregami? Doesn&apos;t
+        the same flat, square sheet of paper yield infinite sculptural
+        possibilities?
       </p>
       <p>
         Although often overlooked,&nbsp;
@@ -43,10 +44,10 @@ export default props => (
       <p>
         Since Quizdini is targeted at the same audience that&nbsp;
         <strong>still uses popscicle sticks</strong> to call upon students at
-        random, we are proud of creating tools that could be
-        characterized as &quot;spartan&quot; and that &nbsp;
-        <strong>call upon teachers&apos; creativity</strong>. After all, we teachers truly shine when we
-        have a little less to work with.
+        random, we are proud of creating tools that could be characterized as
+        &quot;spartan&quot; and that&nbsp;
+        <strong>call upon teachers&apos; creativity</strong>. After all, we
+        teachers truly shine when we have a little less to work with.
       </p>
     </div>
     <div className="belief">
@@ -62,23 +63,25 @@ export default props => (
         They say that &quot;you never stop learning.&quot; If that is true, then
         that must also mean that you never stop receiving feedback. As
         teachers,&nbsp;<strong>we are merely facilitators</strong> of the
-        learning process: a veritable infinite loop of practice interlaced with feedback. Take away formative
-        assessment, especially of the informal variety, and the &quot;wheels of
-        learning&quot; grind to an unceremonious halt.
+        learning process: a veritable infinite loop of practice interlaced with
+        feedback. Take away formative assessment, especially of the informal
+        variety, and the &quot;wheels of learning&quot; grind to an
+        unceremonious halt.
       </p>
       <p>
         In reality, most educators are not teaching rocket science&mdash;neither
-        literally nor figuratively&mdash;so success centers
-        around the ability to orchestrate the learning process in such a way
-        that&nbsp;<strong>every student receives the feedback they need</strong>, how they need it,
-        when they need it. Under ideal circumstances, students themselves would
-        be empowered with tools to track their own progress.
+        literally nor figuratively&mdash;so success centers around the ability
+        to orchestrate the learning process in such a way that&nbsp;
+        <strong>every student receives the feedback they need</strong>, how they
+        need it, when they need it. Under ideal circumstances, students
+        themselves would be empowered with tools to track their own progress.
       </p>
       <p>
         Our goal at Quizdini is to make tools that are consistent with this
         principle. To provide teacher and student alike with tools that
-        &quot;keep the wheels turning.&quot; To us,&nbsp;<strong>empowering teachers</strong> with
-        what they need and&nbsp;<strong>trusting their judgement</strong> is the way forward vis-à-vis
+        &quot;keep the wheels turning.&quot; To us,&nbsp;
+        <strong>empowering teachers</strong> with what they need and&nbsp;
+        <strong>trusting their judgement</strong> is the way forward vis-à-vis
         our embattled Education system.
       </p>
     </div>
@@ -88,32 +91,35 @@ export default props => (
         <Header.Content>&quot;You Gotta Pay to Play&quot;</Header.Content>
       </Header>
       <p>
-        It&apos;s true:&nbsp;<strong>there is no such thing as a free lunch</strong>.
-        In reality, all digital stuff costs money to produce. Notwithstanding
-        labor, there are significant cloud-computing costs, etc., involved in
-        bringing technology to the Interwebs. Unless you want to wake up with a
-        horsehead in your bed,&nbsp;<strong>someone has to bear the costs</strong>.
+        It&apos;s true:&nbsp;
+        <strong>there is no such thing as a free lunch</strong>. In reality, all
+        digital stuff costs money to produce. Notwithstanding labor, there are
+        significant cloud-computing costs, etc., involved in bringing technology
+        to the Interwebs. Unless you want to wake up with a horsehead in your
+        bed,&nbsp;<strong>someone has to bear the costs</strong>.
       </p>
       <p>
         Other ostensibly &quot;free&quot; services, provided by technology
         behemoths&mdash;and those espousing their business models&mdash;require
-        you to&nbsp;<strong>sacrifice your privacy</strong> and, ipso facto, the privacy of your
-        students, to use them.
+        you to&nbsp;<strong>sacrifice your privacy</strong> and, ipso facto, the
+        privacy of your students, to use them.
       </p>
       <p>
-        In truth,&nbsp;<strong>your data is big business</strong>. Always considered yourself kind of
-        a big deal? Well, consider it confirmed! Big Tech uses the information
-        they collect to&nbsp;<strong>track you</strong> and to serve you targeted, behavioral
-        advertising. Oh, and they&nbsp;<strong>sell your data</strong> to a small cadre of shady data
-        brokers too. Admittedly, our technology is simpleton in comparison, but
-        we designed things to provide the&nbsp;<strong>functionality you need without the intrusion</strong>,
-        that frankly, you don&apos;t.
+        In truth,&nbsp;<strong>your data is big business</strong>. Always
+        considered yourself kind of a big deal? Well, consider it confirmed! Big
+        Tech uses the information they collect to&nbsp;
+        <strong>track you</strong> and to serve you targeted, behavioral
+        advertising. Oh, and they&nbsp;<strong>sell your data</strong> to a
+        small cadre of shady data brokers too. Admittedly, our technology is
+        simpleton in comparison, but we designed things to provide the&nbsp;
+        <strong>functionality you need without the intrusion</strong>, that
+        frankly, you don&apos;t.
       </p>
       <p>
-        Our bargain is a simple one:&nbsp;<strong>pay for what you use</strong> and have&nbsp;<strong>peace of
-        mind</strong>. Call it a little old-fashioned, but having a little "skin in the
-        game" can be a good thing. Think of it as supporting "the local paper"
-        of the EdTech world.
+        Our bargain is a simple one:&nbsp;<strong>pay for what you use</strong>{' '}
+        and have&nbsp;<strong>peace of mind</strong>. Call it a little
+        old-fashioned, but having a little "skin in the game" can be a good
+        thing. Think of it as supporting "the local paper" of the EdTech world.
       </p>
     </div>
   </Segment>
