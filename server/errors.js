c7a12c12fcac421ab5be586485d36198ec42800c
James Colestock <james@colestock.com>
2019-11-10 12:33:55 -0700

    Enhancements to error handler, etc.

--- server/errors.js
+++ server/errors.js
@@ -1,52 +1,52 @@
-const { UNAUTHORIZED, BAD_REQUEST } = require('http-status-codes');
+const { BAD_REQUEST, INTERNAL_SERVER_ERROR, UNAUTHORIZED } = require('http-status-codes');
 
 class CustomError extends Error {
-  constructor(msg, code, statusCode) {
+  constructor(msg, statusCode = INTERNAL_SERVER_ERROR, code = undefined) {
     super(msg);
-    this.code = code;
     this.statusCode = statusCode; 
+    this.code = code || this.constructor.name;
   }
 }
 
 class LoginFailed extends CustomError {
   constructor(msg) {
-    super(msg, "LoginFailed", UNAUTHORIZED);
+    super(msg, UNAUTHORIZED);
   }
 }
 
 class DuplicateUsername extends CustomError {
   constructor(msg) {
-    super(msg, "DuplicateUsername", BAD_REQUEST);
+    super(msg, BAD_REQUEST);
   }
 }
 
 class DuplicateEmail extends CustomError {
   constructor(msg) {
-    super(msg,"DuplicateEmail",BAD_REQUEST);
+    super(msg, BAD_REQUEST);
   }
 }
 
 class InsufficientCredits extends CustomError {
   constructor(msg) {
-    super(msg, "InsufficientCredits", BAD_REQUEST);
+    super(msg, BAD_REQUEST);
   }
 }
 
 class InvalidCredentials extends CustomError {
   constructor(msg) {
-    super(msg,"InvalidCredentials", BAD_REQUEST);
+    super(msg, BAD_REQUEST);
   }
 }
 
 class InvalidToken extends CustomError {
   constructor(msg) {
-    super(msg,"InvalidToken", BAD_REQUEST);
+    super(msg, BAD_REQUEST);
   }
 }
 
 class StripeChargeError extends CustomError {
   constructor(msg, code) {
-    super(msg, code, BAD_REQUEST);
+    super(msg, BAD_REQUEST, code);
   }
 }
 
--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -10,7 +10,7 @@ const HttpStatus = require('http-status-codes');
 module.exports = (err, req, res, next) => {
   const statusCode = err.statusCode || HttpStatus.INTERNAL_SERVER_ERROR;
   const message = err.message || HttpStatus.getStatusText(statusCode) || '';
-  const code = err.code || undefined;
+  const code = err.code || err.constructor.name;
   res.status(statusCode).send({ statusCode, message, code });
   next(err);
 };
