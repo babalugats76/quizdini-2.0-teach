338535ae8165af47c0ec09f8dc0b2563de035352
James Colestock <james@colestock.com>
2019-11-19 12:14:10 -0700

    Converted Match component to use hooks

--- client/src/components/Dashboard/MatchCard.js
+++ client/src/components/Dashboard/MatchCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Transition } from 'semantic-ui-react';
 import { copyToClipboard } from './utils';
 import { Button } from '../UI/Button';
@@ -7,110 +7,102 @@ import RouterButton from '../UI/RouterButton';
 
 const { formatDistanceToNow } = require('date-fns');
 
-class MatchCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: true,
-      copied: false
-    };
-  }
+const MatchCard = props => {
 
-  handleExit = event => {
-    this.setState((state, props) => {
-      return { visible: !state.visible };
-    });
+  const [visible, setVisible] = useState(true);
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const copiedTimer = copied && setTimeout(() => {
+      setCopied(prevState => !prevState);
+    }, 2000);
+    return () => clearTimeout(copiedTimer); 
+  }, [copied]);
+
+  const handleExit = event => {
+    setVisible(prevState => !prevState);
   };
 
-  handleHide = event => {
-    const { onMatchDelete } = this.props;
+  const handleHide = event => {
+    const { onMatchDelete } = props;
     onMatchDelete();
   };
 
-  handleCopyUrl = url => {
+  const handleCopyUrl = url => {
     copyToClipboard(url);
-    this.setState((state, props) => {
-      return { copied: true };
-    });
-    setTimeout(() => {
-      this.setState((state, props) => {
-        return { copied: false };
-      });
-    }, 2000);
+    setCopied(true);
   };
 
-  render() {
-    const { matchId, title, updateDate, matches } = this.props.game;
-    const { visible, copied } = this.state;
-    const timeAgo = formatDistanceToNow(Date.parse(updateDate), {
-      addSuffix: false,
-      includeSeconds: false
-    });
-    const url = `${process.env.REACT_APP_GAME_BASE_URL}/match/${matchId}`;
+  const { matchId, title, updateDate, matches } = props.game;
+  const timeAgo = formatDistanceToNow(Date.parse(updateDate), {
+    addSuffix: false,
+    includeSeconds: false
+  });
 
-    return (
-      <Transition
-        animation="fade"
-        duration={1000}
-        onHide={this.handleHide}
-        visible={visible}
-      >
-        <Card className="match-card" key={matchId} raised>
-          <Card.Content className="match-card-header">
-            <Card.Header>{title}</Card.Header>
-            <Button
-              as="button"
+  const url = `${process.env.REACT_APP_GAME_BASE_URL}/match/${matchId}`;
+
+  return (
+    <Transition
+      animation="fade"
+      duration={1000}
+      onHide={handleHide}
+      visible={visible}
+    >
+      <Card className="match-card" key={matchId} raised>
+        <Card.Content className="match-card-header">
+          <Card.Header>{title}</Card.Header>
+          <Button
+            as="button"
+            disabled={!visible}
+            icon="trash"
+            onClick={handleExit}
+            title={`Delete ${title}`}
+            type="button"
+          />
+        </Card.Content>
+        <Card.Content className="match-card-desc">
+          <Card.Description>
+            <Label icon="book">{matches.length} Terms</Label>
+            <Label icon="clock">{timeAgo}</Label>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content className="match-card-btns" extra>
+          <div className="ui three buttons">
+            <RouterButton
+              active
               disabled={!visible}
-              icon="trash"
-              onClick={this.handleExit}
-              title={`Delete ${title}`}
+              icon="edit"
+              pathname="/match"
+              state={{ matchId: matchId }}
+              title={`Edit ${title}`}
+            />
+            <Button
+              active
+              as="a"
+              disabled={!visible}
+              href={url}
+              icon="play"
+              target="_blank"
+              title={`Play ${title}`}
               type="button"
             />
-          </Card.Content>
-          <Card.Content className="match-card-desc">
-            <Card.Description>
-              <Label icon="book">{matches.length} Terms</Label>
-              <Label icon="clock">{timeAgo}</Label>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content className="match-card-btns" extra>
-            <div className="ui three buttons">
-              <RouterButton
-                active
-                disabled={!visible}
-                icon="edit"
-                pathname="/match"
-                state={{ matchId: matchId }}
-                title={`Edit ${title}`}
-              />
-              <Button
-                active
-                as="a"
-                disabled={!visible}
-                href={url}
-                icon="play"
-                target="_blank"
-                title={`Play ${title}`}
-                type="button"
-              />
-              <Button
-                active
-                as="button"
-                className={`clipboard ${copied ? 'copied' : ''}`}
-                disabled={!visible}
-                icon={!copied ? 'link' : null}
-                onClick={() => this.handleCopyUrl(url)}
-                title="Copy URL to clipboard"
-                type="button"
-              >
-                {copied ? 'Copied!' : null}
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </Transition>
-    );
-  }
-}
+            <Button
+              active
+              as="button"
+              className={`clipboard ${copied ? 'copied' : ''}`}
+              disabled={!visible}
+              icon={!copied ? 'link' : null}
+              onClick={() => handleCopyUrl(url)}
+              title="Copy URL to clipboard"
+              type="button"
+            >
+              {copied ? 'Copied!' : null}
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </Transition>
+  );
+};
 
 export default MatchCard;
