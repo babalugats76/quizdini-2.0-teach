b41d9c871bb6e0e23e704cdc793614354a97e785
James Colestock <james@colestock.com>
2019-12-29 09:38:18 -0700

    Switched payments over to use the more stream-lined useData

--- client/src/components/Profile/Payment.js
+++ client/src/components/Profile/Payment.js
@@ -1,42 +1,19 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Segment, Table } from 'semantic-ui-react';
-import { useAPI } from '../../hooks/';
+import { useData } from '../../hooks/';
 import { ExternalLink, Loader } from '../UI/';
 
 const { format } = require('date-fns');
 
 export default props => {
-  // local state - track loading and API response
-  const [state, setState] = useState({
-    loading: false,
-    response: null
+  const { data: payments, error, loading, initialized } = useData({
+    url: '/api/payments',
+    deps: []
   });
 
-  // direct API interactions (ephemeral)
-  const { GET: fetchPayments } = useAPI({
-    url: '/api/payments'
-  });
-
-  // memoized load function
-  const load = useCallback(async () => {
-    setState(state => ({ ...state, loading: true }));
-    const response = await fetchPayments();
-    setState(state => ({ ...state, loading: false, response }));
-  }, [fetchPayments, setState]);
-
-  // fetch payments on mount only
-  useEffect(() => {
-    load();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Destructure and rename data
-  const { loading, response } = state;
-  const { data: payments, error } = response || {};
-  
   // When to show loader
-  const showLoader = loading || !payments;
+  const showLoader = !initialized && (loading || !payments);
 
   return (
     (error && <pre>{JSON.stringify(error, null, 4)}</pre>) ||
