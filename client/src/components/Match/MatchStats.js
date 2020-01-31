6e0dd26a4d8b1617814fca731f0e24fec536237f
James Colestock <james@colestock.com>
2020-01-30 19:56:43 -0700

    added chart.js and started experimenting

--- client/src/components/Match/MatchStats.js
+++ client/src/components/Match/MatchStats.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js";
+
+let myChart;
 
 const MatchStats = props => {
-  return <h1>Grab match stats for...</h1>;
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (typeof myChart !== "undefined") myChart.destroy();
+
+    myChart = new Chart(canvasRef.current, {
+      type: "line",
+      data: {
+        //Bring in data
+        labels: ["1", "2", "3"],
+        datasets: [
+          {
+            label: "My data",
+            data: [10, 20, 30],
+            backgroundColor: "#112233"
+          }
+        ]
+      },
+      options: {
+        //Customize chart options
+      }
+    });
+  }, []);
+
+  return <canvas ref={ref => (canvasRef.current = ref)} />;
 };
 
 export default MatchStats;
